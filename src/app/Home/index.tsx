530ff0fe79d055513eb9f146ce4f943a62771c02
Guilherme Augusto <guilhermedev23@gmail.com>
2025-12-06 23:11:30 -0300

    ok

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text, Image } from "react-native";
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("../../../assets/logo.png")} />
+    </View>
+  );
+}
