612845a8bd05ad4a6bba71e7a0f98a4e11098de1
Guilherme Augusto <guilhermedev23@gmail.com>
2025-12-08 21:18:49 -0300

    ok

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -6,9 +6,15 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
-      <Input  placeholder=" O que Voce Precisa Comprar?"/>
-      <Button title="Entrar" activeOpacity={0.7}
-      />
+      <View style={styles.form}
+      >
+        <Input placeholder=" O que Voce Precisa Comprar?" 
+        />
+        <Button title="Entrar" activeOpacity={0.7}
+
+        />
+      </View>
+      <View style={styles.content}></View>
     </View>
   );
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -7,10 +7,29 @@ export const styles = StyleSheet.create({
         backgroundColor: "#d0d2d8",
         paddingTop: 62,
         padding: 24,
-        gap:24,
+        gap: 24,
     },
+
     logo: {
         height: 34,
         width: 134,
+    },
+
+    form: {
+        width: "100%",
+        paddingHorizontal: 16,
+        gap: 7,
+        marginTop: 42,
+    },
+
+    content: {
+        flex: 1,
+        width: "113%",
+        backgroundColor: "#ffffff",
+        borderTopLeftRadius: 24,
+        borderTopRightRadius: 24,
+        padding: 24,
+        marginTop: 24,
+
     },
 })
