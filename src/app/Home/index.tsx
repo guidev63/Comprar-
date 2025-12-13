50abca7e4a99ec126f23f77249aee66baa208022
Guilherme Augusto <guilhermedev23@gmail.com>
2025-12-12 21:37:52 -0300

    ok

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Image } from "react-native";
+import { View, Image, TouchableOpacity,Text } from "react-native";
 import { styles } from "./styles";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
@@ -6,7 +6,7 @@ import { FilterStatus } from "../types/FilterStatus";
 import { Filter } from "../components/Filter";
 
 
-const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING,FilterStatus.DONE]
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
 
 export function Home() {
   return (
@@ -15,24 +15,30 @@ export function Home() {
         source={require("@/assets/logo.png")}
         style={styles.logo}
 
+
       />
       <View style={styles.form}>
         <Input placeholder="O que você precisa comprar?"
+
         />
+
         <Button title="Entrar"
+
         />
 
       </View>
       <View style={styles.content}>
-        {
-          FILTER_STATUS.map((status) =>(
-           <Filter key={status} status={FilterStatus.DONE} isActive
-        />
-          ))
-        }
-       
+        <View style={styles.header}>
+          {
+            FILTER_STATUS.map((status) => (
+              <Filter key={status} status={status} isActive
 
-       
+              />
+            ))}
+            <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+            </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
+    container:{
         flex: 1,
         alignItems: "center",
         backgroundColor: "#d0d2d8",
@@ -10,24 +10,41 @@ export const styles = StyleSheet.create({
         gap: 24,
     },
 
-    logo: {
+    logo:{
         height: 34,
         width: 134,
     },
 
-    form: {
+    form:{
         width: "100%",
         gap: 7,
         marginTop: 42,
     },
     
-    content: {
+    content:{
         flex: 1,
         width: "113%",
         backgroundColor: "#ffffff",
         borderTopLeftRadius: 24,
         borderTopRightRadius: 24,
         padding: 24,
+        paddingTop: 32,
         marginTop: 24,
+    },
+    header: {
+
+        width:"100%",
+        flexDirection:"row",
+        gap: 12,
+        borderBottomWidth: 1,
+        borderBlockColor: "#E4E6EC",
+        paddingBottom: 12,
+    },
+    clearButton:{
+     marginLeft:"auto",
+    },
+    clearText:{
+    fontSize:12,
+    color:"#828282"
     },
 })
