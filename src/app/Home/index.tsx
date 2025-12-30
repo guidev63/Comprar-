76b179607e4bf03500cb1bf1ac981550f356965a
Guilherme Augusto <guilhermedev23@gmail.com>
2025-12-30 14:27:26 -0300

    ok

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { View, Image, TouchableOpacity, Text, FlatList } from "react-native";
+import { View, Image, TouchableOpacity, Text, FlatList,Alert} from "react-native";
 import { styles } from "./styles";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
@@ -7,37 +7,47 @@ import { FilterStatus } from "../types/FilterStatus";
 import { Filter } from "../components/Filter";
 import { Item } from "../components/Item";
 
+
 const FILTER_STATUS: FilterStatus[] = [
   FilterStatus.PENDING,
   FilterStatus.DONE,
 ];
 
-const ITEMS = [
-
-  { id: "1", status: FilterStatus.DONE, description: "1 Pacote de Café" },
-  { id: "2", status: FilterStatus.PENDING, description: "3 Pacotes de macarrão" },
-  { id: "3", status: FilterStatus.PENDING, description: "3 Cebolas" },
-]
-
 export function Home() {
   const [filter, setFilter] = useState(FilterStatus.PENDING)
-  
   const [description, setDescription] = useState("")
+  const [items,setItems] = useState<any>([])
+  
+  function handleAdd(){
+    if(!description.trim()){
+      return Alert.alert("Adicionar","Informe A Descrição do para Adicionar.")
+    }
 
+    const newItem = {
+       id:Math.random().toString(36).substring(2),
+       description,
+       status: FilterStatus.PENDING,
+    }
+  }
   return (
     <View style={styles.container}>
       <Image
         source={require("@/assets/logo.png")}
         style={styles.logo}
+
       />
       <View style={styles.form}
       >
-        <Input placeholder="O que você precisa Comprar?"
+
+        <Input placeholder="O que Você precisa Comprar?"
          onChangeText={setDescription}
+         
          />
        
-        <Button title="Entrar"
+        <Button title="Adicionar" onPress={handleAdd}
+
         />
+
       </View>
 
       <View style={styles.content}
@@ -53,19 +63,21 @@ export function Home() {
             />
           ))}
 
-          <TouchableOpacity style={styles.clearButton}>
+          <TouchableOpacity style={styles.clearButton}
+          >
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
+
         <FlatList
-          data={ITEMS}
+          data={items}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) =>
           (
             <Item
               data={{ status: item.status, description: item.description }}
-              onStatus={() => console.log("mudar o status")}
-              onRemove={() => console.log("remover")}
+              onStatus={() => console.log("Mudar O Status")}
+              onRemove={() => console.log("Remover")}
             />
           )}
           showsVerticalScrollIndicator={false}
